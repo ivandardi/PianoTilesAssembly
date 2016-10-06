3f7ad67b450c2cbfc14e90d7527cd10979c44eba
Ivan Dardi <ivy.dardi@gmail.com>
2016-10-06 11:23:23 -0300

    Reorganize project, add runnable jar, add better README, add piano sound

--- README.md
+++ README.md
@@ -1,10 +1,32 @@
-# PianoTilesAssembly
-Clone do jogo "Piano Tiles" em MIPS Assembly para o projeto final de Arquitetura e Organização de Computadores
+MIPS-PIANO TILES
+==========
 
-## COMO RODAR
+Clone do jogo "Piano Tiles" em MIPS Assembly para o projeto final de Arquitetura e Organização de Computadores.
 
-Vá em `Tools -> Bitmap Display`. Coloque o width em 256, o height em 512 e o base address em `0x10040000 (heap)`.
 
-Clique em "Connect to MIPS".
+## How To Run
 
-Abra o arquivo `main.asm` no MARS. Aperte F3 (Run -> Assemble). Depois aperte F5 (Run -> Go).
+1. Open the Mars .jar file from our repo.
+2. Load the main.asm file into Mars with File -> Open.
+3. Go to Run -> Assemble (or press F3)
+4. Go to tools -> Bitmap Display
+5. The Bitmap Display settings should be as follows:
+  - Unit Width: 8
+  - Unit Height: 8
+  - Display Width: 256
+  - Display Height: 512
+  - Base Address: heap
+6. Go to tools -> Keyboard and Display MMIO Simulator
+7. Press `Connect to MIPS` on both of the displays
+8. Go to Run -> Go (or press F5)
+9. All controls should take place in the lower portion of the Keyboard and Display Simulator
+
+## Controls
+
+The piano tiles can be played by pressing the keys respective to the column that has a black tile currently at the bottom.
+
+```
+Column | 1 | 2 | 3 | 4 |
+       -----------------
+Key    | f | g | h | j |
+```
---
+++ main.asm
@@ -0,0 +1,151 @@
+# PIANO TILES IN MIPS ASSEMBLY
+#
+# A tela é de 32 x 64
+#
+
+
+
+#
+# CONSTANTS
+#
+
+    # Endereco de inicio da tela
+    .eqv SCREEN_BEGIN 0x10040000
+
+    # Endereco de fim da tela
+    .eqv SCREEN_END 0x10044000
+
+    # Largura do retangulo do piano
+    .eqv RECT_WIDTH 8
+
+    # Altura do retangulo do piano
+    .eqv RECT_HEIGHT 16
+
+    # Largura da tela
+    .eqv SCREEN_WIDTH 32
+
+    # Largura da tela
+    .eqv SCREEN_HEIGHT 64
+
+
+
+#
+# MACROS
+#
+
+    # Macro que para a execução do programa
+    .macro done
+        li $v0,10
+        syscall
+    .end_macro
+
+    # Macro de início de função
+    .macro FUNCTION_BEGIN (%name)
+        %name :
+    .end_macro
+
+    # Macro de fim de função
+    .macro FUNCTION_END
+        jr $ra
+    .end_macro
+
+    # Macro que toca nota de piano. pitch = 60 significa C
+    .macro PLAY_NOTE (%pitch)
+        li $v0, 33
+        li $a0, %pitch
+        li $a1, 1200
+        li $a2, 0
+        li $a3, 0xFFFF
+        syscall
+    .end_macro
+
+
+
+#
+# PROGRAM
+#
+
+.data
+
+.text
+
+
+main:
+    li $a0, 0x00FFFFFF
+    jal ClearScreen
+
+    li $a0, 1
+    li $a1, 3
+    li $a2, 0x000000FF
+    jal DrawRect
+
+    PLAY_NOTE(60)
+    PLAY_NOTE(62)
+    PLAY_NOTE(63)
+    PLAY_NOTE(65)
+    PLAY_NOTE(67)
+    PLAY_NOTE(69)
+    PLAY_NOTE(70)
+    PLAY_NOTE(72)
+
+    done
+
+
+
+# Funcao do gameloop
+#
+#
+FUNCTION_BEGIN Gameloop
+
+FUNCTION_END
+
+
+
+# Função de limpar a tela
+# $a0: cor
+FUNCTION_BEGIN ClearScreen
+    li    $t0, SCREEN_BEGIN # iterator
+    li    $t1, SCREEN_END   #end value of the for loop
+.forloop:
+    sub   $t2, $t0, $t1     # $t1 is bit flag
+    beqz  $t2, .endforloop
+    sw    $a0, 0($t0)       # Set $t0 to the color stored in $a0
+    addiu $t0, $t0, 4       # Increment %t0 by 4
+    j     .forloop
+.endforloop:
+FUNCTION_END
+
+
+
+# Funcao que desenha um retangulo na posicao (x, y), com a largura 8 e altura 16
+# $a0: x
+# $a1: y
+# $a2: cor
+FUNCTION_BEGIN DrawRect
+    add $t0, $a0, RECT_WIDTH
+    add $t1, $a1, RECT_HEIGHT
+DrawRect.forloop1:
+    sub  $v0, $t1, $a1
+    beqz $v0, DrawRect.endforloop1
+
+    move $t9, $a0
+DrawRect.forloop2:
+    sub  $v0, $t0, $t9
+    beqz $v0, DrawRect.endforloop2
+
+    move $t8, $a1                # $t8 = y
+    rol  $t8, $t8, 5             # $t8 = y  * SCREEN_WIDTH
+    add  $t8, $t8, $t9           # $t8 = y  * SCREEN_WIDTH + x
+    rol  $t8, $t8, 2             # $t8 = (y * SCREEN_WIDTH + x) * 4
+    addi $t8, $t8, SCREEN_BEGIN  # $t8 = (y * SCREEN_WIDTH + x) * 4 + SCREEN_BEGIN
+    sw   $a2, ($t8)              # tela[y*w + x] = $a2
+
+    addi $t9, $t9, 1
+    j DrawRect.forloop2
+DrawRect.endforloop2:
+
+    addi $a1, $a1, 1
+    j DrawRect.forloop1
+DrawRect.endforloop1:
+FUNCTION_END
+
