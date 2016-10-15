2a37f3ce47317a521eeac9a05a617ccbf06a69cc
Ivan Dardi <ivy.dardi@gmail.com>
2016-10-15 19:52:54 -0300

    Fiz gameloop label

--- main.asm
+++ main.asm
@@ -160,7 +160,7 @@
 main:
     CLEAR_SCREEN(COR_SCREEN)
     
-    la  $a0, hbty
+    la  $a0, ttls
     jal Gameloop
 
     DONE
@@ -216,7 +216,7 @@ Gameloop.input:
 
     # If tile is 0, then the song ended
     lw   $t0, 0($s1)
-    beqz $t0, Gameloop.endsong
+    beqz $t0, Gameloop.end
 
     #
     # DISPLAY
